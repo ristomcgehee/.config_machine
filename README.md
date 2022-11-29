37b189d905f255992b30e3ef78b07a9538635b47
Chris McGehee <cmcgehee@datto.com>
2022-11-29 06:51:37 -0800

    2

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # .config_machine
 ```
+sudo apt-get install curl -y
 curl https://raw.githubusercontent.com/chrismcgehee/.config_machine/main/init_machine.sh | bash
 ```
--- init_machine.sh
+++ init_machine.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+set -e # Exit if any command fails
+
+sudo apt-get install - y \
+    git \
+    python3-apt \
+    python3-pip
 sudo pip3 install ansible
-sudo apt-get install git -y
+
 mkdir -p ~/.config/chrismcgehee/.config_machine
-git clone https://github.com/chrismcgehee/.config_machine.git 
+git clone https://github.com/chrismcgehee/.config_machine.git ~/.config/chrismcgehee/.config_machine
