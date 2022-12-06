572179f73a40fe1d12cdd79013d81abd15e5120a
Chris McGehee <cmcgehee@datto.com>
2022-12-06 07:30:39 -0800

    30

--- playbooks/files/write_lyncser_config.py
+++ playbooks/files/write_lyncser_config.py
@@ -3,8 +3,14 @@
 import base64
 import json
 import os
+import subprocess
 
-os.chdir(os.environ['HOME'] + '/.config/lyncser')
+# Run a shell command
+def run(cmd):
+    return subprocess.run(cmd, shell=True, check=True, capture_output=True).stdout.decode('utf-8').strip()
+
+home_dir = os.path.expanduser('~' . run('logname'))
+os.chdir(home_dir)
 lyncser_json_base64 = os.environ['LYNCSER_JSON_BASE64']
 lyncser_json = base64.b64decode(lyncser_json_base64).decode('utf-8')
 for key, value in json.loads(lyncser_json).items():
