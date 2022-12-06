37dea3df1fff12e8092d994e5ac082c4b49cb1e3
Chris McGehee <cmcgehee@datto.com>
2022-12-06 07:18:55 -0800

    29

--- execute_playbooks.sh
+++ execute_playbooks.sh
@@ -5,7 +5,7 @@ set -o pipefail # Exit if any command in a pipeline fails
 
 source export_env.sh
 if [ ! -f "$HOME/.config/lyncser/globalConfig.yaml" ]; then
-    sudo ansible-playbook -i hosts playbooks/lyncser.yaml
+    sudo ansible-playbook -i hosts playbooks/lyncser.yml
     source "$HOME/.bashrc"
 fi
 ls -p playbooks/ | grep -v / | grep -v lyncser | xargs -I{} sudo ansible-playbook -i hosts playbooks/{}
