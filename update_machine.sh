e9875bdaa724350366e325f42865e3a7d09b69b8
Chris McGehee <cmcgehee@datto.com>
2022-12-05 19:02:00 -0800

    8

--- update_machine.sh
+++ update_machine.sh
@@ -4,4 +4,5 @@ set -e # Exit if any command fails
 
 CONFIG_DIR=~/.config/chrismcgehee/.config_machine
 cd $CONFIG_DIR
+git pull --rebase
 ls playbooks/ | xargs -I{} sudo ansible-playbook -i hosts playbooks/{}
