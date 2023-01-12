03df6445424ca3872a148879de96b5c2f8261f66
Risto McGehee <risto.mcgehee@gmail.com>
2023-01-11 20:13:58 -0800

    2

--- execute_playbooks.sh
+++ execute_playbooks.sh
@@ -3,9 +3,16 @@
 set -e # Exit if any command fails
 set -o pipefail # Exit if any command in a pipeline fails
 
+hosts_file=$1
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
+
+sudo pip install -r requirements.txt
+
 source export_env.sh
 if [ ! -f "$HOME/.config/lyncser/globalConfig.yaml" ]; then
-    sudo ansible-playbook -i hosts playbooks/lyncser.yml
+    sudo ansible-playbook -i $hosts_file playbooks/lyncser.yml
     source "$HOME/.bashrc"
 fi
-ls -p playbooks/ | grep -v / | grep -v lyncser | xargs -I{} sudo ansible-playbook -i hosts playbooks/{}
+ls -p playbooks/ | grep -v / | grep -v lyncser | xargs -I{} sudo ansible-playbook -i $hosts_file playbooks/{}
